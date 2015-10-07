d3c1130baf424c7e83a86ecc90d85733a844a18d
Michal Špaček <tupinek@gmail.com>
2015-10-07 23:09:06 +0200

    Prepare for 0.07 version.

--- Service.pm
+++ Service.pm
@@ -12,7 +12,7 @@ use Error::Pure qw(err);
 use File::Spec::Functions qw(catfile);
 
 # Version.
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Construct.
 sub new {
@@ -303,6 +303,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
