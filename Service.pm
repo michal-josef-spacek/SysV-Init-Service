40e34869b24142151333ee830ac8382e613a4c1d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-12-06 00:28:04 +0100

    Update author name

--- Service.pm
+++ Service.pm
@@ -287,13 +287,13 @@ L<https://github.com/michal-josef-spacek/SysV-Init-Service>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
-© 2013-2023 Michal Špaček
+© 2013-2023 Michal Josef Špaček
 
 BSD 2-Clause License
 
